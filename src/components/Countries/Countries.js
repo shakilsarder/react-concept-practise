921371bc7cb559b11915c53162cc9fff2c14305d
Shakilk Sarder <shakil01748@hotmail.com>
2022-03-25 21:48:58 +0600

    first commit

---
+++ src/components/Countries/Countries.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react';
+import Country from '../Country/Country';
+import './Countries.css'
+const Countries = () => {
+    const [countries, setCountries] = useState([]);
+    useEffect(() => {
+        fetch('https://restcountries.com/v3.1/all')
+            .then(res => res.json())
+            .then(data => setCountries(data));
+    }, []);
+    return (
+        <div>
+            <div className='Countries'>
+                {
+                    countries.map(country => <Country country={country}
+                        key={country.cca3}> </Country>)
+                }
+            </div>
+        </div >
+    );
+};
+
+export default Countries;
